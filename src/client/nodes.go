d0d7715c2f2517046258cf804a008daec0a00c01
Artem Klyukvin <a.klyukvin@joom.com>
2021-09-05 12:13:14 +0300

    add first working lambda

--- src/client/nodes.go
+++ src/client/nodes.go
@@ -3,5 +3,6 @@ package client
 import "github.com/Sovianum/figma-search-app/src/figma/figmaclient"
 
 type Node struct {
-	ID figmaclient.NodeID `json:"id"`
+	NodeID figmaclient.NodeID `json:"nodeId"`
+	User   string             `json:"user"`
 }
